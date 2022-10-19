a7cbf154d016424cc916dc07acd68fd7f89538ad
Marcicley Fonseca <marcicleytfonseca@gmail.com>
2022-10-19 14:45:17 -0300

    Arquivo C++ para teste consolidado

---
+++ Trabalho colaborativo.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+	printf("\n\t\t>>>TRABALHO COLABORATIVO E MÍDIAS DIGITAIS<<<\n");
+	printf("\n\t\t>>>>>>Alimentando um repositório remoto<<<<<<\n");
+	printf("\n\t\tMarcicley Tomás da Fonseca - 20222tsis2744\n");
+	return 0;
+}
